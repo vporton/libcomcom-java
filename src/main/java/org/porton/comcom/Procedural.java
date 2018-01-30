a462d96537d1a5822e4123acd3d8768b35d487c3
Victor Porton <porton@narod.ru>
2018-01-31 00:22:13 +0200

    developing

--- src/main/java/org/porton/comcom/Procedural.java
+++ src/main/java/org/porton/comcom/Procedural.java
@@ -5,6 +5,8 @@
  */
 package org.porton.comcom;
 
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.PointerByReference;
 import java.util.Map;
 import org.portonvictor.errno.Errno;
 
@@ -13,6 +15,14 @@ import org.portonvictor.errno.Errno;
  * @author Victor Porton
  */
 public class Procedural {
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                terminate();
+            }
+        });
+    }
 
     static public void init() {
         if(Bindings.CLibrary.INSTANCE.libcomcom_init() == -1)
@@ -26,10 +36,25 @@ public class Procedural {
             Map<String,String> env,
             int timeout)
     {
+        PointerByReference output;
+        Bindings.size_t output_len;
+        /* FIXME */
+        Bindings.CLibrary.INSTANCE.libcomcom_run_command(
+                input,
+                input.length,
+                output,
+                Pointer.NULL,
+                input,
+                argv,
+                envp,
+                timeout);
+        if(output_len.longValue() > Integer.MAX_VALUE)
+            throw new OutOfMemoryError(); // correct?
+        return output.getValue().getByteArray(0, output_len.intValue());
     }
 
     static public void terminate() {
-        if(Bindings.CLibrary.INSTANCE.libcomcom_terminate()== -1)
+        if(Bindings.CLibrary.INSTANCE.libcomcom_terminate() == -1)
             throw new Errno();
     }
 
